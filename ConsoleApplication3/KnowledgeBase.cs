4b64e444023353dad3bc967ebef61f7cf222d7af
andrewkn <a.knight@hotmail.com.au>
2015-05-18 17:05:21 +1000

    Started Knowledge BAse

--- ConsoleApplication3/KnowledgeBase.cs
+++ ConsoleApplication3/KnowledgeBase.cs
@@ -9,9 +9,17 @@ namespace InferenceEngine
     {
         private Proposition[] _Propostions;
 
+        public int Length
+        {
+            get {
+                return _Propostions.Length;
+            }
+        }
+
         public KnowledgeBase(InferenceEngine.Proposition[] Porpositions)
         {
             throw new System.NotImplementedException();
+            
         }
 
         public void IsTrue(int PropositionNo, bool[] Arguements)
